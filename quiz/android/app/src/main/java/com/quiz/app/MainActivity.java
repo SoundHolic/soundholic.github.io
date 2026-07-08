f6199fd0be63f11d9cdd3f1061e544c92254904a
ShuaiHong <shuaihong@yahoo.com>
2026-07-08 23:23:35 +0800

    Add Android project: MainActivity.java

---
+++ quiz/android/app/src/main/java/com/quiz/app/MainActivity.java
@@ -0,0 +1,50 @@
+package com.quiz.app;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private static final String QUIZ_URL = "https://soundholic.github.io/quiz/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        // Dark background for loading
+        webView.setBackgroundColor(Color.parseColor("#0a0a0f"));
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(false);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
+        // Hide scrollbar
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        webView.loadUrl(QUIZ_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = (WebView) findViewById(android.R.id.content);
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
